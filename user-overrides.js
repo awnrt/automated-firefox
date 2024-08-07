9616f99b44a8b40727d68a6eacf7f99e5fbc6198
awy <awy@tutamail.com>
2024-08-07 04:52:54 +0300

    compact mode

--- user-overrides.js
+++ user-overrides.js
@@ -29,3 +29,4 @@ user_pref("permissions.default.microphone", 2);
 
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.compactmode.show", true);
+user_pref("browser.uidensity", 1);
